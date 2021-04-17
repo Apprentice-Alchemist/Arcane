691e190c2a200c58d146ddd44543e8c00f9ea953
Apprentice Alchemist <azzer159@yahoo.com>
2021-04-17 11:02:03 +0200

    ensure runner exits with 0 exit code on success

--- tests/Main.hx
+++ tests/Main.hx
@@ -4,14 +4,29 @@ import utest.ui.text.PlainTextReport;
 import utest.Runner;
 
 class Main {
-	public static function main() {
+	public static function main() try {
 		var runner = new Runner();
 		runner.addCases(common);
-		var report = new PlainTextReport(runner, report -> arcane.util.Log.println(report.getResults()));
+		var report = new PlainTextReport(runner, report -> {
+			arcane.util.Log.println(report.getResults());
+			throw new ExitException();
+		});
 		@:privateAccess {
 			report.newline = "\n";
 			report.indent = "    ";
 		}
 		runner.run();
+	} catch(e:ExitException) {};
+}
+
+#if (haxe_ver > "4.1.0")
+class ExitException extends haxe.Exception {
+	override public function new() {
+		super("");
 	}
 }
+#else
+class ExitException {
+	public function new(){}
+}
+#end
